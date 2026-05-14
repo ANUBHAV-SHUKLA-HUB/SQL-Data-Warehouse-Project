807152024b1591c7c3aac331139932c5ba2597fa
ANUBHAV-SHUKLA-HUB <anubhavshukla.lev@gmail.com>
2026-05-14 09:54:02 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -18,7 +18,8 @@ CREATE TABLE Silver.crm_cust_info(
 	cst_lastname		NVARCHAR(50),
 	cst_marital_status  NVARCHAR(50),
 	cst_gndr			NVARCHAR(50),
-	cst_create_date		DATE
+	cst_create_date		DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 IF OBJECT_ID ('Silver.crm_prd_info','U') IS NOT NULL
